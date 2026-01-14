679f9af1868b8f4154932d63cdbcc8fe487277bf
numbersguy104 <numbersboy@icloud.com>
2026-01-14 13:49:33 -0500

    Implement player magnet controls
    The player can control their magnetism using up (positive) and down (negative) on a joystick or with arrow keys. Player magnetism value is accessible with the GetMagnetState() method.

---
+++ IGME603Project1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("How far the right stick needs to be pushed up or down to activate the magnet.\nShould be a value between 0 (stick just barely pushed up or down) and 1 (stick pushed all the way up or down).")]
+    [SerializeField] private const float _magnetJoystickThreshold = 0.1f;
+
+    private MagnetState _magnetState = MagnetState.Neutral;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    //Gets the player's current magnet state (Neutral / Positive / Negative)
+    public MagnetState GetMagnetState() {
+        return _magnetState;
+    }
+
+    //Sets the player's current magnet state (Neutral / Positive / Negative)
+    public void SetMagnetState(MagnetState ms)
+    {
+        _magnetState = ms;
+
+        //Change the player's color based on the new state
+        switch(ms)
+        {
+            case MagnetState.Positive:
+                gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                break;
+            case MagnetState.Negative:
+                gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                break;
+            default:
+                gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                break;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        //currently unused, will eventually handle crawling
+    }
+
+    public void UpdateMagnet(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            float magnetism = context.ReadValue<float>();
+            if (magnetism > _magnetJoystickThreshold)
+            {
+                SetMagnetState(MagnetState.Positive);
+            }
+            else if (magnetism < -_magnetJoystickThreshold)
+            {
+                SetMagnetState(MagnetState.Negative);
+            }
+            else
+            {
+                SetMagnetState(MagnetState.Neutral);
+            }
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            SetMagnetState(MagnetState.Neutral);
+        }
+    }
+}
---
+++ IGME603Project1/Assets/Scripts/Utilities/MagnetState.cs
@@ -0,0 +1,6 @@
+public enum MagnetState
+{
+    Neutral,
+    Positive,
+    Negative
+}
